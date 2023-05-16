e547eb3b200d5b2c9c8628c61c6dace2538199b7
Desing01 <Desing01@BENLEE>
2023-05-17 00:50:50 +0300

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _1_CSharp_MerhabaDunya
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Merhaba Dünya 1. Satır.");
+            Console.Write("Merhaba Dünya");
+
+            Console.ReadKey();
+        }
+    }
+}
